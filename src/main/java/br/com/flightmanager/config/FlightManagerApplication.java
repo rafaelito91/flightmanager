c6df9c892fd2d4a75ce4826b9517713d75836f74
Rafael Ito <rafaelito91@gmail.com>
2018-04-03 17:47:44 -0300

    Polish auto generated package name and add javadoc

--- src/main/java/br/com/flightmanager/config/FlightManagerApplication.java
+++ src/main/java/br/com/flightmanager/config/FlightManagerApplication.java
@@ -1,8 +1,14 @@
-package br.com.flightmanager.Flight.Manager;
+package br.com.flightmanager.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Responsible for starting the spring boot application
+ *
+ * @author Rafael Kyoshi Ito
+ * @version 1.0 03/04/18
+ */
 @SpringBootApplication
 public class FlightManagerApplication {
 
