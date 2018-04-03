a42daee1f442847cb57006c2fcf45ac033c9cde1
Rafael Ito <rafaelito91@gmail.com>
2018-04-03 17:28:25 -0300

    Created Spring Boot project with Spring Initializr tool

---
+++ src/main/java/br/com/flightmanager/Flight/Manager/FlightManagerApplication.java
@@ -0,0 +1,12 @@
+package br.com.flightmanager.Flight.Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightManagerApplication.class, args);
+	}
+}
